5f567d3a3105ff5c7c1b6250c17eecaaa9d16d08
Tabitha <tabithahsia@gmail.com>
2017-09-17 23:06:16 -0700

    bugs bugs bugs

--- server/index.js
+++ server/index.js
@@ -9,17 +9,19 @@ var port = process.env.PORT || 7485;
 app.use(bodyParser.json() );
 app.use(express.static(__dirname + '/../client/dist'));
 
+app.get('/getFact', db.getRandomFact)
 
-app.get('/getFact', function (req, res) {
-  db.getRandomFact(function(err, data) {
-    if(err){
-      res.send(501, err);
-    } else {
-      // console.log(data);
-      res.send(data[0].fact);
-    }
-  })
-})
+/*** MYSQL version ***/
+// app.get('/getFact', function (req, res) {
+//   db.getRandomFact(function(err, data) {
+//     if(err){
+//       res.send(501, err);
+//     } else {
+//       // console.log(data);
+//       res.send(data[0].fact);
+//     }
+//   })
+// })
 
 app.listen(port, function () {
   console.log('listening on port TABS');
